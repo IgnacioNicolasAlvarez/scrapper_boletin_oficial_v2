ada1f769ce71d68839571b69291e82cf61625da5
Ignacio Nicolás Alvarez <ignacionicolasalvarez@gmail.com>
2021-11-10 22:31:06 -0300

    se agrega findchilder, relacion 1 1 1

--- src/core/scrapper.py
+++ src/core/scrapper.py
@@ -1,5 +1,4 @@
-import mechanicalsoup as ms
-
+from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
 
@@ -10,13 +9,22 @@ payload = f"fechaBoletin={today}"
 
 headers = {
     "Content-Type": "application/x-www-form-urlencoded",
-    "Cookie": "ci_session=fvbho0m1t7q0nhalep2jkgcpuhm761hg",
 }
 
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+# class="float-end mb-1" volver a 
+# class="font-monospace" texto del aviso
+# class="blog-post-title" titulo del aviso
+# class="class="pt-5 pb-4 mb-4 fst-italic border-bottom" numero del aviso
 
 def run():
     response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
-
-
-# browser = ms.StatefulBrowser()
+    soup = BeautifulSoup(response.text, "html.parser")
+    notices = soup.find_all("article", class_="blog-post")[1]
+    #c = [notices[i:i + 4] for i in range(0, len(notices), 4)]
+    print(notices.findChildren("h3", class_="pt-5 pb-4 mb-4 fst-italic border-bottom"))
+    print(notices.findChildren("h2", class_="blog-post-title"))
+    print(notices.findChildren("p", class_="font-monospace"))
