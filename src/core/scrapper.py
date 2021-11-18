da503cfb5ac324877fa172f4687a67bcb3f265d6
Ignacio Nicolás Alvarez <ignacionicolasalvarez@gmail.com>
2021-11-17 22:59:42 -0300

    se agrega dict

--- src/core/scrapper.py
+++ src/core/scrapper.py
@@ -11,20 +11,28 @@ headers = {
     "Content-Type": "application/x-www-form-urlencoded",
 }
 
-def chunks(lst, n):
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-# class="float-end mb-1" volver a 
-# class="font-monospace" texto del aviso
-# class="blog-post-title" titulo del aviso
-# class="class="pt-5 pb-4 mb-4 fst-italic border-bottom" numero del aviso
 
 def run():
     response = requests.request("POST", url, headers=headers, data=payload)
     soup = BeautifulSoup(response.text, "html.parser")
+
     notices = soup.find_all("article", class_="blog-post")[1]
-    #c = [notices[i:i + 4] for i in range(0, len(notices), 4)]
-    print(notices.findChildren("h3", class_="pt-5 pb-4 mb-4 fst-italic border-bottom"))
-    print(notices.findChildren("h2", class_="blog-post-title"))
-    print(notices.findChildren("p", class_="font-monospace"))
+    titles = notices.findChildren(
+        "h3", class_="pt-5 pb-4 mb-4 fst-italic border-bottom"
+    )
+    subtitles = notices.findChildren("h2", class_="blog-post-title")
+    bodies = notices.findChildren("p", class_="font-monospace")
+
+    if len(titles) != len(subtitles) != len(bodies):
+        print("Error en el scrapper")
+        return
+
+    dict_advice = []
+    for i in range(len(titles)):
+        title = titles[i].text.strip()
+        subtitle = subtitles[i].text.strip()
+        body = bodies[i].text.strip()
+
+        dict_advice.append({"title": title, "subtitle": subtitle, "body": body})
+
+    print(dict_advice)
